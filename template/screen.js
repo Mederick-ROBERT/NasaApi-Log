1376c181a6f6500f9d435e3fb8c48beed1d9b962
Mederick-ROBERT <mederick.robert@free.fr>
2023-05-08 20:39:21 +0200

    navbar responsive

---
+++ template/screen.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button')
+const list = document.getElementById('hiddenNavbar')
+
+function navbar(){
+    list.classList.toggle('hiddenNavbar')
+    list.classList.toggle('active');
+}
