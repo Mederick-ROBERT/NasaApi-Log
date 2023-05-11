6bde210990c0bfed76247152b9c1d45036a8a13c
Mederick-ROBERT <mederick.robert@free.fr>
2023-05-11 18:55:41 +0200

    création du dossier log

---
+++ Log/index.php
@@ -0,0 +1,20 @@
+<head>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<?php
+include "../template/header.html"
+?>
+
+<body class="whiteChange">
+   
+<section id="wrapper">
+
+    
+
+</section>
+
+</body>
+
+<script src="../JS/flout.js"></script>
+<script src="../JS/blackHole.js"></script>
